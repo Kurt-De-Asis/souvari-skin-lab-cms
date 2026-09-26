fb2f487ca7f9d216b11079ee2fc3d6ce7abc3df0
Kurt Russel De Asis <kurtrussel644@gmail.com>
2026-09-26 14:39:41 +0800

    Fix refresh page issue

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -6,10 +6,14 @@ const api = axios.create({
   headers: { 'Content-Type': 'application/json' },
 });
 
-let accessToken: string | null = null;
+const TOKEN_KEY = 'auth_token';
+
+let accessToken: string | null = localStorage.getItem(TOKEN_KEY);
 
 export function setAccessToken(token: string | null) {
   accessToken = token;
+  if (token) localStorage.setItem(TOKEN_KEY, token);
+  else localStorage.removeItem(TOKEN_KEY);
 }
 
 export function getAccessToken() {
